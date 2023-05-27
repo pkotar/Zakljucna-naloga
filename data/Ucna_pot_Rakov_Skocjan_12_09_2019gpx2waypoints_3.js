b0949ea813d5fa21e1bccefea3ee48160a2c61ec
pkotar <134282906+pkotar@users.noreply.github.com>
2023-05-27 18:30:03 +0200

    Add files via upload

---
+++ data/Ucna_pot_Rakov_Skocjan_12_09_2019gpx2waypoints_3.js
@@ -0,0 +1,1 @@
+var json_Ucna_pot_Rakov_Skocjan_12_09_2019gpx2waypoints_3 = {"type":"FeatureCollection","name":"Ucna_pot_Rakov_Skocjan_12_09_2019gpx2waypoints_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
